f4788caaeccb58c5d0f048c31761d0fd0e716fc0
Alexsandro Souza <alexsandro.souza@deem.com>
2020-08-30 18:04:34 +0100

    Remove grpc reference from swagger ui

--- services/swaggerui/config.go
+++ services/swaggerui/config.go
@@ -4,36 +4,26 @@ import (
 	"crypto/tls"
 	"github.com/apssouza/openapi/certtls"
 	_ "github.com/apssouza22/protobuf-gen-code/statik"
-	"github.com/gorilla/mux"
 	"github.com/rakyll/statik/fs"
 	"github.com/sirupsen/logrus"
-	grpc_client "gitlab.com/deem-devops/phoenix/grpc-go/client"
-	"google.golang.org/grpc"
 	"mime"
 	"net/http"
-	"strings"
 )
 
 const addr = "0.0.0.0:8081"
 
-func StartHTTPServer(builder grpc_client.GrpcClientBuilder) {
-	conn, err := builder.GetConn("localhost", "8080")
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	searchHandler := getSvcReqHandler(conn)
+func StartHTTPServer() {
 	swaggerHandler := getSwaggerHandler()
 
 	gwServer := &http.Server{
 		Addr:    addr,
-		Handler: getRequestHandler(searchHandler, swaggerHandler),
+		Handler: getRequestHandler(swaggerHandler),
 	}
 	gwServer.TLSConfig = &tls.Config{
 		Certificates: []tls.Certificate{certtls.Cert},
 	}
-	logrus.Infof("Http server started on port 8081")
+	logrus.Infof("Http server started on %s", addr)
 	gwServer.ListenAndServeTLS("", "")
-
 }
 
 // getSwaggerHandler serves an Swagger UI.
@@ -47,20 +37,8 @@ func getSwaggerHandler() http.Handler {
 	return http.FileServer(statikFS)
 }
 
-func getSvcReqHandler(conn *grpc.ClientConn) *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	//apiHandler := book.GetServiceReqHandler(conn)
-	//router.HandleFunc("/apis/v1/bookStore.ListBooks", apiHandler.ListBooks).Methods("POST")
-	//router.HandleFunc("/apis/v1/bookStore.CreateBook", apiHandler.CreateBook).Methods("POST")
-	return router
-}
-
-func getRequestHandler(searchHandler http.Handler, swaggerHandler http.Handler) http.HandlerFunc {
+func getRequestHandler(swaggerHandler http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasPrefix(r.URL.Path, "/apis") {
-			searchHandler.ServeHTTP(w, r)
-			return
-		}
 		swaggerHandler.ServeHTTP(w, r)
 	}
 }
